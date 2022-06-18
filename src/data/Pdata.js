163b52781ebaa56d03537501f1a66194890b5b6c
mgorem <oremgitonga22@gmail.com>
2022-06-18 14:51:52 +0300

    modify projects latest project

--- src/data/Pdata.js
+++ src/data/Pdata.js
@@ -3,11 +3,11 @@ import {v4 as uuidv4} from "uuid"
 export const Pdata = [
     {
         id: uuidv4(),
-        title: "Project One",
-        desc: "Lorem ipsum dolor sit amet consectetur adipiscing elit",
-        github: "https://github.com/mgorem",
-        demo: "https://oremgitonga.co.ke",
-        image: "./images/project-1.jpg"
+        title: "Ecommerce Website",
+        desc: "An Full Stack Ecommerce Website Application using React, Node.js and Mongo DB",
+        github: "https://github.com/mgorem/ecommerce-react-node-mongo",
+        demo: "https://mgs-fineline.co.ke",
+        image: "./images/home-part-2-men.jpg"
     },
     {
         id: uuidv4(),
